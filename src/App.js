6e3ac8d771c92d754f2fe5696582b6b8eb7056bb
seha3 <silviaherrera33@gmail.com>
2020-02-02 00:15:49 -0600

    added a song in html

--- src/App.js
+++ src/App.js
@@ -1,16 +1,44 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
 import Search from './Components/search'
 import SearchEpisode from './Components/searchEpisode'
+import Header from './Components/header'
 
-function App() {
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom'
+
+function App () {
   return (
-   <div>
-     <Search />
-     <SearchEpisode />
-
-   </div>
-  );
+    <Router>
+      <Header />
+      <div>
+        <div>
+          <nav>
+            <div>
+              <Link to='/characters' className='heading2'>Search</Link>
+            </div>
+            <div>
+              <Link to='/episodes' className='heading2'>SearchEpisode</Link>
+            </div>
+          </nav>
+        </div>
+        <span>
+          <Switch>
+            <Route path='/characters'>
+              <Search />
+            </Route>
+            <Route path='/episodes'>
+              <SearchEpisode />
+            </Route>
+          </Switch>
+        </span>
+      </div>
+    </Router>
+  )
 }
 
-export default App;
+export default App
---
+++ src/Components/header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Header extends React.Component {
+  render () {
+    return (
+      <header>
+        <h1 className='heading'>Rick <span>And</span> Morty</h1>
+      </header>
+    )
+  }
+}
+
+export default Header
