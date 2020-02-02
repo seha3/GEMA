325e571450d7ddae58642176a7d5db49f3f2f546
seha3 <silviaherrera33@gmail.com>
2020-02-01 21:25:44 -0600

    episodes searchbar added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Search from './Components/search'
+import SearchEpisode from './Components/searchEpisode'
 
 function App() {
   return (
    <div>
-     <Search/>
+     <Search />
+     <SearchEpisode />
 
    </div>
   );
